d44cbb6059dbd81ab0dce6ed13274352b5cf8d5e
Obafemi Ogunmokun <femzy123@yahoo.com>
2024-06-06 02:42:09 +0100

    useFormStatus

--- components/todo-form.tsx
+++ components/todo-form.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { addTodo } from "@/app/todos/actions";
 import { Button } from "@/components/ui/button";
@@ -6,17 +6,20 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea";
 import { Send } from "lucide-react";
 import { useRef } from "react";
+import { useFormStatus } from "react-dom";
 
 function FormContent() {
+  const { pending } = useFormStatus();
   return (
     <>
       <Textarea
+        disabled={pending}
         minLength={4}
         name="todo"
         required
         placeholder="Add a new todo"
       />
-      <Button type="submit" size="icon" className="min-w-10">
+      <Button type="submit" size="icon" className="min-w-10" disabled={pending}>
         <Send className="h-5 w-5" />
         <span className="sr-only">Submit Todo</span>
       </Button>
@@ -25,14 +28,18 @@ function FormContent() {
 }
 
 export function TodoForm() {
-  const formRef = useRef<HTMLFormElement>(null)
+  const formRef = useRef<HTMLFormElement>(null);
   return (
     <Card>
       <CardContent className="p-3">
-        <form ref={formRef} className="flex gap-4" action={async (data) => {
-          await addTodo(data)
-          formRef.current?.reset()
-        }}>
+        <form
+          ref={formRef}
+          className="flex gap-4"
+          action={async (data) => {
+            await addTodo(data);
+            formRef.current?.reset();
+          }}
+        >
           <FormContent />
         </form>
       </CardContent>
--- components/todo-item.tsx
+++ components/todo-item.tsx
@@ -7,6 +7,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
 import { Todo } from "@/types/custom";
 import { Trash2 } from "lucide-react";
+import { useFormStatus } from "react-dom";
 
 export function TodoItem({ todo }: { todo: Todo }) {
   return (
@@ -17,19 +18,29 @@ export function TodoItem({ todo }: { todo: Todo }) {
 }
 
 export function TodoCard({ todo }: { todo: Todo }) {
+  const {pending} = useFormStatus()
   return (
-    <Card className={cn("w-full")}>
+    <Card className={cn("w-full", pending && "opacity-50")}>
       <CardContent className="flex items-start gap-3 p-3">
         <span className="size-10 flex items-center justify-center">
-          <Checkbox checked={Boolean(todo.is_complete)} onCheckedChange={async (val) => {
-            if(val === "indeterminate") return
-            await updateTodo({...todo, is_complete: val})
-          }} />
+          <Checkbox
+            disabled={pending}
+            checked={Boolean(todo.is_complete)}
+            onCheckedChange={async (val) => {
+              if (val === "indeterminate") return;
+              await updateTodo({ ...todo, is_complete: val });
+            }}
+          />
         </span>
         <p className={cn("flex-1 pt-2 min-w-0 break-words")}>{todo.task}</p>
-        <Button variant="ghost" size="icon" formAction={async (data) => {
-          await deleteTodo(todo.id)
-        }}>
+        <Button
+          disabled={pending}
+          variant="ghost"
+          size="icon"
+          formAction={async (data) => {
+            await deleteTodo(todo.id);
+          }}
+        >
           <Trash2 className="h-5 w-5" />
           <span className="sr-only">Delete Todo</span>
         </Button>
