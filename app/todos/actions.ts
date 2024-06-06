933e1c94bbf659e51764e750b9df012c27f5a0a9
Obafemi Ogunmokun <femzy123@yahoo.com>
2024-06-06 02:06:29 +0100

    Update and Delete Todo

--- app/todos/actions.ts
+++ app/todos/actions.ts
@@ -32,3 +32,46 @@ export async function addTodo(formData: FormData) {
   revalidatePath("/todos");
 }
 
+export async function deleteTodo(id: number) {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    throw new Error("User is not logged in");
+  }
+
+  const { error } = await supabase.from("todos").delete().match({
+    user_id: user.id,
+    id: id,
+  });
+
+  if (error) {
+    throw new Error("Error deleting task");
+  }
+
+  revalidatePath("/todos");
+}
+
+export async function updateTodo(todo: Todo) {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    throw new Error("User is not logged in");
+  }
+
+  const { error } = await supabase.from("todos").update(todo).match({
+    user_id: user.id,
+    id: todo.id,
+  });
+
+  if (error) {
+    throw new Error("Error updating task");
+  }
+
+  revalidatePath("/todos");
+}
