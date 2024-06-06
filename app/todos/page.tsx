cd099128a2b97ceefc5d174d4215bc30e3f678f9
Obafemi Ogunmokun <femzy123@yahoo.com>
2024-06-06 01:41:28 +0100

    Create and Read Todos

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -4,17 +4,20 @@ import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
 export default async function TodosPage() {
-  const supabase = await createClient()
+  const supabase = await createClient();
 
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
   if (!user) {
-    return redirect('/login')
+    return redirect("/login");
   }
 
-  const todos = ["This is a todo"];
+  const {data: todos} = await supabase
+    .from("todos")
+    .select()
+    .order("inserted_at", { ascending: false });
 
   return (
     <section className="p-3 pt-6 max-w-2xl w-full flex flex-col gap-4">
